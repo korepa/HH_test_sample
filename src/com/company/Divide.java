4fe9da028e918e18f0e0c81def06ef83f5d3ae9d
Knyazev Ivan <to.knyazev@gmail.com>
2014-09-16 00:27:20 +0400

    6
    Объединили

--- src/com/company/Divide.java
+++ src/com/company/Divide.java
@@ -1,19 +1,57 @@
 package com.company;
-import java.util.Arrays;
 
 // класс, определящий значение дроби
 // вход - массив А
 // вход - массив В
+// вход - разрядность системы счисления
 public class Divide {
+    int first;
+    int second;
+    int rank;
 
-    // переменные (массивы А и В)
-    int[] arrayFirst, arraySecond;
-    double divide;
+    // конструктор
+    public Divide(int _a, int _b, int _rank){
+        first = _a;
+        second = _b;
+        rank = _rank;
+    }
 
-    public Divide(int[] _arrayFirst, int[] _arraySecond)
-    {
-        // инициализация массивов
-        arrayFirst  = _arrayFirst;
-        arraySecond  = _arraySecond;
+    // функция для расчета значения дроби
+    public String Calculate() {
+        // вычисляем значение
+        String rightResult = "";
+        String leftResult = "";
+        // количество шагов
+        int steps = 10;
+        int step = 0;
+
+        // вычисляем дробь в десятичной системе отсчета
+        double num = (double) first / second;
+        int leftPart = (int)num;
+        double rightPart = num - leftPart;
+        num = rightPart;
+
+        // вычисляем целую часть в цикле
+        int value = leftPart;
+        while ( value % rank > 0 ) {
+
+            int p = value / rank;
+            int q = value % rank;
+            leftResult = q + leftResult;
+            value = p;
+        }
+        if (leftResult == "")
+            leftResult = "0";
+
+        // вычисляем дробную часть в цикле, пробегаем с указанной точностью
+        while(step++<= steps)
+        {
+            num = num * rank;
+            rightResult += Integer.parseInt(String.valueOf((int)Math.floor(num)),rank);
+            num = num - Math.floor(num);
+        }
+
+        // сложим правую и левую части
+        return leftResult + "," + rightResult;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,5 @@
 package com.company;
-import java.io.Console;
+
 import java.util.*;
 import java.util.Arrays;
 
@@ -9,34 +9,79 @@ public class Main {
     // главная функция
     public static void main(String[] args) {
 
-        // пример выполнения функции
-        System.out.format("Пример нахождения медианы для массива 2N (N > 0)\n");
+        // выберем тип задания (1 - вычисление медианы, 2 - вычисление дроби в k-ной системе счисления)
+        System.out.println("Введите действие: 1 - вычисление медианы, 2 - вычисление дроби в k-ной системе счисления. Другой символ - для выхода");
+        Scanner sc = new Scanner(System.in);
+        String type = sc.next();
 
-        // (пример)объявили 2 массива
-        int[] arrayFirst = {1, 2, 3, 4};
-        int[] arraySecond = {1, 4, 5, 6};
+        if (type.equals("1")){
+            // выбрали вычисление медианы
 
-        // посчитаем медиану
-        Median median = new Median(arrayFirst, arraySecond);
-        median.CalculateMedian();
-        median.OutputResult();
+            // пример выполнения функции
+            System.out.format("Пример нахождения медианы для массива 2N (N > 0)\n");
 
-        // выполнения функции с вводом данных
-        System.out.format("Нахождение медианы для массива 2N (N > 0)\n");
+            // (пример)объявили 2 массива
+            int[] arrayFirst = {1, 2, 3, 4};
+            int[] arraySecond = {1, 4, 5, 6};
 
-        // выполнение функции с вводом данных от пользователя (сначала массив А, потом В)
-        arrayFirst = inputUserArray();
-        arraySecond = inputUserArray();
-
-        // сверим размеры массивов
-        if (arrayFirst.length == arraySecond.length){
-            // посчитаем медиану для пользовательского ввода
-            median = new Median(arrayFirst, arraySecond);
+            // посчитаем медиану
+            Median median = new Median(arrayFirst, arraySecond);
             median.CalculateMedian();
             median.OutputResult();
+
+            // выполнения функции с вводом данных
+            System.out.format("Нахождение медианы для массива 2N (N > 0)\n");
+
+            // выполнение функции с вводом данных от пользователя (сначала массив А, потом В)
+            arrayFirst = inputUserArray();
+            arraySecond = inputUserArray();
+
+            // сверим размеры массивов
+            if (arrayFirst.length == arraySecond.length){
+                // посчитаем медиану для пользовательского ввода
+                median = new Median(arrayFirst, arraySecond);
+                median.CalculateMedian();
+                median.OutputResult();
+            }
+            else {
+                System.out.format("Размеры массивов не совпадают. Массив А[%d]. Массив В[%d].\n", arrayFirst.length, arraySecond.length);
+            }
+
         }
-        else {
-            System.out.format("Размеры массивов не совпадают. Массив А[%d]. Массив В[%d].\n", arrayFirst.length, arraySecond.length);
+        else if (type.equals("2")){
+            // выбрали вычисление дроби в k-ной системе счисления
+
+            // используемые переменные
+            Divide data;
+            String result;
+
+            // текстовое сообщение
+            System.out.println("Пример деления в k-ичной системе счисления");
+
+            // примеры 1
+            System.out.println("Первый ввод (1-число, 2-число, разрядность): 1 2 8");
+            data = new Divide(1,2,8);
+            result = data.Calculate();
+            System.out.format("Результат дроби a/b: %s\n", result);
+
+            // примеры 2
+            System.out.println("Второй ввод (1-число, 2-число, разрядность): 1 12 10");
+            data = new Divide(1,12,10);
+            result = data.Calculate();
+            System.out.format("Результат дроби a/b: %s\n", result);
+
+            // обрабатываем пользовательский ввод
+            // ввод 1
+            System.out.println("Первый ввод (1-число, 2-число, разрядность) через пробел:");
+            data = inputBlock();
+            result = data.Calculate();
+            System.out.format("Результат дроби a/b: %s\n", result);
+
+            // ввод 2
+            System.out.println("Второй ввод (1-число, 2-число, разрядность) через пробел:");
+            data = inputBlock();
+            result = data.Calculate();
+            System.out.format("Результат дроби a/b: %s\n", result);
         }
 
         // для выхода нажмите кнопку
@@ -52,12 +97,12 @@ public class Main {
     // функция для ввода пользователем массива
     static int[] inputUserArray(){
         List<Integer> list = new ArrayList<Integer>();
-        Scanner stdin = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Начало ввода целочисленного натурального массива. Для завершения ввода введите символ - не число (например, n):");
         do {
             System.out.println("Ввод числа. Текущий массив: " + list);
-            String symb = stdin.next();
+            String symb = sc.next();
             if (!symb.startsWith("n")) {
                 try {
                     // преобразуем ввод
@@ -82,5 +127,50 @@ public class Main {
         }
         System.out.println("Полученный массив: " + Arrays.toString(arr));
         return arr;
+    }
+
+    // функция для расчета значения дроби
+    static Divide inputBlock() {
+
+        // объявим класс
+        Divide data;
+        int a, b, div;
+
+        // вычисляем значение
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Введите первое число (делимое)...");
+        a=getInt(sc);
+        System.out.println("Введите второе число (делитель)...");
+        b=getInt(sc);
+        System.out.println("Введите степень исчисления...");
+        div=getInt(sc);
+
+        // полученные значения
+        System.out.format("Полученные значения: %d, %d, %d\n", a,   b,  div );
+        data = new Divide(a, b, div);
+        return data;
+    }
+
+    private static int getInt(Scanner sc)
+    {
+        for (;;) {
+            if (!sc.hasNextInt()) {
+                System.out.println("Вводите только целые числа больше 0!");
+                sc.next();
+                continue;
+            }
+            int value = sc.nextInt();
+            if (value >= 0) {
+                return value;
+
+            } else {
+                System.out.print("Некорректный ввод!");
+
+            }
+            break;
+        }
+
+        return 0;
     }
 }
